578c17d794280465e0c28d80091480a942d04ceb
Dwinger2006 <familiedwinger@t-online.de>
2024-09-22 21:28:26 +0200

    Issues with tranfer of coordinates

--- L2LuxGeo.user.js
+++ L2LuxGeo.user.js
@@ -2,7 +2,7 @@
 // @name         WME Link to Geoportal Luxembourg
 // @description  Adds a button to Waze Map Editor to open the Geoportal of Luxembourg with the coordinates of the current WME location.
 // @namespace    https://github.com/Dwinger2006/Dancingman81   
-// @version      2024.09.22.11
+// @version      2024.09.22.12
 // @include      https://*.waze.com/editor*
 // @include      https://*.waze.com/*/editor*
 // @grant        none
@@ -12,7 +12,6 @@
 (function() {
     'use strict';
 
-    // Function to extract query string from URL
     function getQueryString(link, name) {
         var pos = link.indexOf(name + '=') + name.length + 1;
         var len = link.substr(pos).indexOf('&');
@@ -20,41 +19,37 @@
         return link.substr(pos, len);
     }
 
-    // Function to adjust zoom for Geoportal Luxembourg
     function adjustZoomForGeoportal(wmeZoom) {
-        return wmeZoom;  // Passe den Zoom-Level für das Geoportal an
+        return wmeZoom; 
     }
 
-    // Create the button for Luxembourg Geoportal
     function createLuxButton() {
         var lux_btn = $('<button style="width: 285px;height: 24px; font-size:85%;color: Green;border-radius: 5px;border: 0.5px solid lightgrey; background: white">Geoportal Luxemburg</button>');
         
         lux_btn.click(function() {
             var href = $('.WazeControlPermalink a').attr('href');
 
-            var lon = parseFloat(getQueryString(href, 'lon'));  // WGS84 Längengrad
-            var lat = parseFloat(getQueryString(href, 'lat'));  // WGS84 Breitengrad
+            var lon = parseFloat(getQueryString(href, 'lon')); 
+            var lat = parseFloat(getQueryString(href, 'lat')); 
             var zoom = parseInt(getQueryString(href, 'zoom')) + CorrectZoom(href);
 
             zoom = adjustZoomForGeoportal(zoom);
 
             console.log("WGS84 Koordinaten: Längengrad =", lon, "Breitengrad =", lat);
 
-            // Check if Proj4js is available before transforming coordinates
             if (typeof proj4 !== 'undefined') {
                 try {
-                    // WGS84 Koordinaten (Längengrad/Breitengrad)
                     var wgs84Proj = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs";
+                    var lurefProj = "+proj=lcc +lat_1=49.5 +lat_2=50.83333333333334 +lat_0=49 +lon_0=6 +x_0=80000 +y_0=100000 +ellps=GRS80 +units=m +no_defs";
 
-                    // LUREF Projektion (Luxemburg)
-                    var lurefProj = "+proj=lcc +lat_1=49.833333 +lat_2=51.166667 +lat_0=49 +lon_0=6 +x_0=80000 +y_0=100000 +ellps=GRS80 +units=m +no_defs";
-
-                    // Umwandlung von WGS84 (Längengrad/Breitengrad) in LUREF (X, Y)
                     var luref = proj4(wgs84Proj, lurefProj, [lon, lat]);
+
+                    if (!luref || luref.length !== 2 || isNaN(luref[0]) || isNaN(luref[1])) {
+                        throw new Error("Ungültige LUREF-Koordinaten");
+                    }
 
                     console.log("Umgewandelte LUREF-Koordinaten: X =", luref[0], "Y =", luref[1]);
 
-                    // Erstelle die Geoportal-URL mit den umgewandelten LUREF-Koordinaten
                     var mapsUrl = 'https://map.geoportail.lu/theme/main?lang=de&version=3&zoom=' + zoom + '&X=' + luref[0] + '&Y=' + luref[1] + '&rotation=0&layers=549-542-302-269-320-2056-351-152-685-686&opacities=1-0-0-0-1-0-1-1-1-1&time=------------------&bgLayer=streets_jpeg&crosshair=true';
 
                     window.open(mapsUrl, '_blank');
@@ -69,18 +64,15 @@
         return lux_btn;
     }
 
-    // Insert the button into the WME sidepanel
     function addButton() {
         if (document.getElementById('user-info') == null) {
             setTimeout(addButton, 500);
-            console.log('user-info element not yet available, page still loading');
             return;
         }
 
-        // Check if the button already exists
         if (document.getElementById("sidepanel-lux") !== null) {
             console.log("Button already exists.");
-            return; // Exit if the button is already present
+            return;
         }
 
         var addon = document.createElement('section');
@@ -105,7 +97,6 @@
 
     addButton();
 
-    // Load Proj4js dynamically in advance
     (function loadProj4() {
         var script = document.createElement("script");
         script.type = 'text/javascript';
@@ -121,7 +112,6 @@
         };
     })();
 
-    // Function to correct zoom level based on whether it's a livemap or editor
     function CorrectZoom(link) {
         var found = link.indexOf('livemap');
         return (found === -1) ? 13 : 2;

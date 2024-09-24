4d142dbe9ba51228bc506aaee7741455a04c9115
Dwinger2006 <familiedwinger@t-online.de>
2024-09-24 22:01:49 +0200

    Fehlerbehebung bei der Anzeige von L2LuxGeo

--- L2LuxGeo.user.js
+++ L2LuxGeo.user.js
@@ -2,7 +2,7 @@
 // @name         WME Link to Geoportal Luxembourg and Traffic Info
 // @description  Adds buttons to Waze Map Editor to open the Geoportal of Luxembourg and the Luxembourg traffic info portal.
 // @namespace    https://github.com/Dwinger2006/Dancingman81   
-// @version      2024.09.22.14
+// @version      2024.09.24.2
 // @include      https://*.waze.com/editor*
 // @include      https://*.waze.com/*/editor*
 // @grant        none
@@ -23,37 +23,54 @@
 (function() {
     'use strict';
 
-// Function to create Luxembourg Geoportal Button
-function createLuxButton() {
-    console.log("Creating Geoportal Luxemburg button");
-    var lux_btn = $('<button style="width: 285px;height: 24px; font-size:85%;color: Green;border-radius: 5px;border: 0.5px solid lightgrey; background: white; margin-bottom: 10px;">Geoportal Luxemburg</button>');
+    // Function to extract parameters from URL
+    function getQueryString(url, param) {
+        var params = new URLSearchParams(url.split('?')[1]);
+        return params.get(param);
+    }
 
-    lux_btn.click(function() {
-        var href = $('.WazeControlPermalink a').attr('href');
-        var lon = parseFloat(getQueryString(href, 'lon')); 
-        var lat = parseFloat(getQueryString(href, 'lat')); 
-        var zoom = parseInt(getQueryString(href, 'zoom')) + CorrectZoom(href);
+    // Function to adjust zoom for Geoportal Luxembourg
+    function adjustZoomForGeoportal(wmeZoom) {
+        return wmeZoom;  // Passe den Zoom-Level für das Geoportal an
+    }
 
-        zoom = adjustZoomForGeoportal(zoom);
+    // Function to correct zoom level based on whether it's a livemap or editor
+    function CorrectZoom(link) {
+        var found = link.indexOf('livemap');
+        return (found === -1) ? 13 : 2;
+    }
 
-        // Option: Koordinaten von WGS84 zu LUREF umrechnen
-        var wgs84Proj = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs";
-        var lurefProj = "+proj=lcc +lat_1=49.833333 +lat_2=51.166667 +lat_0=49 +lon_0=6 +x_0=80000 +y_0=100000 +ellps=GRS80 +units=m +no_defs";
-        
-        var luref = proj4(wgs84Proj, lurefProj, [lon, lat]);
+    // Function to create Luxembourg Geoportal Button
+    function createLuxButton() {
+        console.log("Creating Geoportal Luxemburg button");
+        var lux_btn = $('<button style="width: 285px;height: 24px; font-size:85%;color: Green;border-radius: 5px;border: 0.5px solid lightgrey; background: white; margin-bottom: 10px;">Geoportal Luxemburg</button>');
 
-        console.log("LUREF Koordinaten:", luref);
+        lux_btn.click(function() {
+            var href = $('.WazeControlPermalink a').attr('href');
+            var lon = parseFloat(getQueryString(href, 'lon')); 
+            var lat = parseFloat(getQueryString(href, 'lat')); 
+            var zoom = parseInt(getQueryString(href, 'zoom')) + CorrectZoom(href);
 
-        // Verwende die umgerechneten Koordinaten in der URL
-        var mapsUrl = 'https://map.geoportail.lu/theme/main?lang=de&version=3&zoom=' + zoom + '&X=' + luref[0] + '&Y=' + luref[1] + '&rotation=0&layers=549-542-302-269-320-2056-351-152-685-686&opacities=1-0-0-0-1-0-1-1-1-1&time=------------------&bgLayer=streets_jpeg&crosshair=true';
-        
-        console.log("Geoportal URL:", mapsUrl);
+            zoom = adjustZoomForGeoportal(zoom);
 
-        window.open(mapsUrl, '_blank');
-    });
+            // Option: Koordinaten von WGS84 zu LUREF umrechnen
+            var wgs84Proj = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs";
+            var lurefProj = "+proj=lcc +lat_1=49.833333 +lat_2=51.166667 +lat_0=49 +lon_0=6 +x_0=80000 +y_0=100000 +ellps=GRS80 +units=m +no_defs";
+            
+            var luref = proj4(wgs84Proj, lurefProj, [lon, lat]);
 
-    return lux_btn;
-}
+            console.log("LUREF Koordinaten:", luref);
+
+            // Verwende die umgerechneten Koordinaten in der URL
+            var mapsUrl = 'https://map.geoportail.lu/theme/main?lang=de&version=3&zoom=' + zoom + '&X=' + luref[0] + '&Y=' + luref[1] + '&rotation=0&layers=549-542-302-269-320-2056-351-152-685-686&opacities=1-0-0-0-1-0-1-1-1-1&time=------------------&bgLayer=streets_jpeg&crosshair=true';
+            
+            console.log("Geoportal URL:", mapsUrl);
+
+            window.open(mapsUrl, '_blank');
+        });
+
+        return lux_btn;
+    }
 
     // Function to create Luxembourg Traffic Info Button
     function createTrafficButton() {

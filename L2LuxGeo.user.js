b5677a75f2c400ee68c5d9c25dde7ca7f9f5e7e8
Dwinger2006 <familiedwinger@t-online.de>
2024-09-24 21:56:13 +0200

    Korrektur 1. Button

--- L2LuxGeo.user.js
+++ L2LuxGeo.user.js
@@ -23,25 +23,37 @@
 (function() {
     'use strict';
 
-    // Function to create Luxembourg Geoportal Button
-    function createLuxButton() {
-        console.log("Creating Geoportal Luxemburg button");
-        var lux_btn = $('<button style="width: 285px;height: 24px; font-size:85%;color: Green;border-radius: 5px;border: 0.5px solid lightgrey; background: white; margin-bottom: 10px;">Geoportal Luxemburg</button>');
+// Function to create Luxembourg Geoportal Button
+function createLuxButton() {
+    console.log("Creating Geoportal Luxemburg button");
+    var lux_btn = $('<button style="width: 285px;height: 24px; font-size:85%;color: Green;border-radius: 5px;border: 0.5px solid lightgrey; background: white; margin-bottom: 10px;">Geoportal Luxemburg</button>');
+
+    lux_btn.click(function() {
+        var href = $('.WazeControlPermalink a').attr('href');
+        var lon = parseFloat(getQueryString(href, 'lon')); 
+        var lat = parseFloat(getQueryString(href, 'lat')); 
+        var zoom = parseInt(getQueryString(href, 'zoom')) + CorrectZoom(href);
+
+        zoom = adjustZoomForGeoportal(zoom);
+
+        // Option: Koordinaten von WGS84 zu LUREF umrechnen
+        var wgs84Proj = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs";
+        var lurefProj = "+proj=lcc +lat_1=49.833333 +lat_2=51.166667 +lat_0=49 +lon_0=6 +x_0=80000 +y_0=100000 +ellps=GRS80 +units=m +no_defs";
         
-        lux_btn.click(function() {
-            var href = $('.WazeControlPermalink a').attr('href');
-            var lon = parseFloat(getQueryString(href, 'lon')); 
-            var lat = parseFloat(getQueryString(href, 'lat')); 
-            var zoom = parseInt(getQueryString(href, 'zoom')) + CorrectZoom(href);
+        var luref = proj4(wgs84Proj, lurefProj, [lon, lat]);
 
-            zoom = adjustZoomForGeoportal(zoom);
+        console.log("LUREF Koordinaten:", luref);
 
-            var mapsUrl = 'https://map.geoportail.lu/theme/main?lang=de&version=3&zoom=' + zoom + '&X=' + lon + '&Y=' + lat + '&rotation=0&layers=549-542-302-269-320-2056-351-152-685-686&opacities=1-0-0-0-1-0-1-1-1-1&time=------------------&bgLayer=streets_jpeg&crosshair=true';
-            window.open(mapsUrl, '_blank');
-        });
+        // Verwende die umgerechneten Koordinaten in der URL
+        var mapsUrl = 'https://map.geoportail.lu/theme/main?lang=de&version=3&zoom=' + zoom + '&X=' + luref[0] + '&Y=' + luref[1] + '&rotation=0&layers=549-542-302-269-320-2056-351-152-685-686&opacities=1-0-0-0-1-0-1-1-1-1&time=------------------&bgLayer=streets_jpeg&crosshair=true';
+        
+        console.log("Geoportal URL:", mapsUrl);
 
-        return lux_btn;
-    }
+        window.open(mapsUrl, '_blank');
+    });
+
+    return lux_btn;
+}
 
     // Function to create Luxembourg Traffic Info Button
     function createTrafficButton() {

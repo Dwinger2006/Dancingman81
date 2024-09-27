d4f1b1f4a10023fd183fb637b32704d9d9ec8a46
Dwinger2006 <familiedwinger@t-online.de>
2024-09-27 23:26:03 +0200

    Skript wurde falsch im WME angezeigt

--- L2LuxGeo.user.js
+++ L2LuxGeo.user.js
@@ -2,7 +2,7 @@
 // @name         WME Link to Geoportal Luxembourg and Traffic Info
 // @description  Adds buttons to Waze Map Editor to open the Geoportal of Luxembourg and the Luxembourg traffic info portal.
 // @namespace    https://github.com/Dwinger2006/Dancingman81   
-// @version      2024.09.26.2
+// @version      2024.09.27.2
 // @include      https://*.waze.com/editor*
 // @include      https://*.waze.com/*/editor*
 // @grant        none
